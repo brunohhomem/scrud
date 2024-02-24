f9bef58052d4b8fddf0771b77c5884facbf68c73
Bruno H. Homem <brunohhomem@gmail.com>
2024-02-24 16:09:33 -0300

    configs

---
+++ scrud/src/main/java/com/bhh/scrud/ScrudApplication.java
@@ -0,0 +1,13 @@
+package com.bhh.scrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrudApplication.class, args);
+	}
+
+}
