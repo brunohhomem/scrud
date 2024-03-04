6279e0b0566b59c10296794a01bc480447145d39
Bruno H. Homem <brunohhomem@gmail.com>
2024-03-04 18:46:30 -0300

    added: Category PUT

--- scrud/src/main/java/com/bhh/scrud/controllers/CategoryController.java
+++ scrud/src/main/java/com/bhh/scrud/controllers/CategoryController.java
@@ -1,13 +1,13 @@
 package com.bhh.scrud.controllers;
 
+import com.bhh.scrud.dtos.CategoryDTO;
+import com.bhh.scrud.dtos.ProductsDTO;
 import com.bhh.scrud.entities.Category;
 import com.bhh.scrud.services.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,5 +28,11 @@ public class CategoryController {
     public ResponseEntity<Optional<Category>> findById(@PathVariable Long id){
         Optional<Category> category = service.findById(id);
         return ResponseEntity.ok().body(category);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody @Valid CategoryDTO dto){
+        dto = service.update(id, dto);
+        return ResponseEntity.ok().body(dto);
     }
 }
--- scrud/src/main/java/com/bhh/scrud/services/CategoryService.java
+++ scrud/src/main/java/com/bhh/scrud/services/CategoryService.java
@@ -1,9 +1,13 @@
 package com.bhh.scrud.services;
 
+import com.bhh.scrud.dtos.CategoryDTO;
+import com.bhh.scrud.dtos.ProductsDTO;
 import com.bhh.scrud.entities.Category;
+import com.bhh.scrud.entities.Products;
 import com.bhh.scrud.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,4 +20,16 @@ public class CategoryService {
     public List<Category> getAll() { return repository.findAll(); }
 
     public Optional<Category> findById(Long id) {return repository.findById(id);}
+
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+        Category entity = repository.getReferenceById(id);
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new CategoryDTO(entity);
+    }
+
+    private void copyDtoToEntity(CategoryDTO dto, Category entity) {
+        entity.setName(dto.getName());
+    }
 }
